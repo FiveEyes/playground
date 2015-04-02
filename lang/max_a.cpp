b69a5cbef433182a536d96411f0d67d2be6918ed
FiveEyes <findthegateopen@gmail.com>
2015-04-01 17:28:54 -0700

    update

--- lang/max_a.cpp
+++ lang/max_a.cpp
@@ -1,24 +1,39 @@
 //given b, satisfy a * (a + b) = c * c, maximize a.
+//http://poj.openjudge.cn/practice/1046/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
 
 int main() {
-  int b, w;
-  while(scanf("%d", &b) != EOF) {
-    w = 1;
-    while(!(b & 1)) {
-      w *= 2;
+  int t;
+  cin >> t;
+  while(t--) {
+    ll a = 0, b, n;
+    cin >> b;
+    
+    if(b & 1) {
+      n = (b * b + 1) / 2;
+    } else {
       b /= 2;
+      if(b & 1) {
+        n = (b * b + 1);
+      } else {
+        n = (b * b / 2 + 2);
+      }
+      b *= 2;
     }
-    int n = (b * b + 1) / 2 * w;
-    b *= w;
-    int a = (n - b) / 2;
-    int c2 = a * (a + b);
-    int c = sqrt(c2);
-    printf("%d %d %d\n", a, c2, c * c);
-
+    a = (n - b) / 2;
+    //ll c2 = a * (a + b);
+    //ll c = sqrt(c2);
+    //printf("%d %d %d\n", a, c2, c * c);
+    cout << a << endl;
   }
   return 0;
 }
