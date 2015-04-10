533de2bc516e7e0678e4519e460a08b43adeccdf
FiveEyes <findthegateopen@gmail.com>
2015-04-09 20:56:38 -0700

    update frp.hs

--- lang/frp.hs
+++ lang/frp.hs
@@ -11,26 +11,26 @@ nagInfinity = -1.0 / 0.0 :: Time
 type B a = Time -> a
 type E a = [(Time, a)]
 
-newtype Behavior a = 
+newtype Behavior a =
   Behavior {
     at :: B a
   }
-  
-newtype Event a = 
+
+newtype Event a =
   Event {
     occs :: E a
   }
-  
+
 time::Behavior Time
 time = Behavior id
 
 instance Functor Behavior where
   fmap f ba = Behavior $ fmap f (at ba)
-  
+
 instance Applicative Behavior where
-  pure a = Behavior (const a) 
-  (<*>) fab fa = Behavior $ (at fab) <*> (at fa)   
-  
+  pure a = Behavior (const a)
+  (<*>) fab fa = Behavior $ (at fab) <*> (at fa)
+
 lift2 :: (a1 -> a2 -> b) -> Behavior a1 -> Behavior a2 -> Behavior b
 lift2 f ba1 ba2 = f <$> ba1 <*> ba2
 
@@ -47,21 +47,29 @@ merge ((t0,a0):xs) ((t1,a1):ys)
 instance Monoid (Event a) where
   mempty = Event []
   mappend l r = Event $ merge (occs l) (occs r)
-  
+
 instance Functor Event where
   fmap f ea = Event $ map (\(t, a) -> (t, f a)) (occs ea)
- 
+
 delayOccs :: (Time, Event a) -> E a
 delayOccs (t, e) = [(max t ta, a) | (ta, a) <- occs e]
- 
+
 joinE :: Event (Event a) -> Event a
 joinE ee = Event $ foldr merge [] $ map delayOccs (occs ee)
 
 instance Monad Event where
   return a = Event [(nagInfinity, a)]
   (>>=) ea f = joinE $ fmap f ea
-  
+
 instance Applicative Event where
   pure = return
   (<*>) = ap
 
+before :: E a -> Time -> [a]
+before os t = [a | (ta, a) <- os, ta < t]
+
+switcher :: Behavior a -> Event (Behavior a) -> Behavior a
+switcher b0 e = Behavior (\t -> at (last (b0 : before (occs e) t)) t)
+
+stepper :: a -> Event a -> Behavior a
+stepper a0 e = switcher (pure a0)  (pure <$> e)
